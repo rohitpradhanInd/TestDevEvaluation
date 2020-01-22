a38b8223f1aa4daae937ace33cba6d2bb677d95a
“Rohit.pradhan” <rohitpradhan60@gmail.com>
2020-01-22 15:22:28 +0530

    Location changed

---
+++ Posts/AppDelegate/AppDelegate.swift
@@ -0,0 +1,34 @@
+//  AppDelegate.swift
+//  Posts
+//
+//  Created  on 11/01/20.
+//
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+ 
+
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
